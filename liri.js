7555af945af9b42b60e213f58674a88169957af9
Scott Thomas <scottthomas@Scotts-MacBook-Pro.local>
2018-02-03 18:16:16 -0700

    added a little work on twitter api call

--- liri.js
+++ liri.js
@@ -3,4 +3,22 @@
 var keys = require("./keys.js");
 var twitter = require("twitter");
 var request = require("request");
-var spotify = require("spotify");
+var spotify = require("node-spotify-api");
+var inquirer = require("inquirer");
+
+
+inquirer
+  .prompt([{
+    type: "input",
+    name: "userInput",
+    message: "Hi, I'm Liri! What would you like to do?"
+  }])
+  .then(function(inquirerResponse) {
+    if (inquirerResponse.userInput === "node liri my-tweets") {
+      client.get('search/tweets', {
+        q: 'node.js'
+      }, function(error, tweets, response) {
+        console.log(tweets);
+      });
+    };
+  });
