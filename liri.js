a02e398522255b5c423b24628b1ee1e308731af9
Scott Thomas <scottthomas@Scotts-MacBook-Pro.local>
2018-02-03 17:44:30 -0700

    Added our package-lock.json and installed our node dependencies

---
+++ liri.js
@@ -0,0 +1,6 @@
+// Make our global variables and grab our API's
+
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var request = require("request");
+var spotify = require("spotify");
