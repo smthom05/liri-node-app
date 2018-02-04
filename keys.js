213a43d9e75e822b702b4b424e6b92bc430955bd
Scott Thomas <scottthomas@Scotts-MacBook-Pro.local>
2018-02-03 17:34:58 -0700

    added Twitter API information

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '0Q5BRUCtE7EEvJeEO32fMVltj',
+  consumer_secret: 'eAcwUWIsGX6aVWELkDzhpBCldgLTny6KrBKu0HIKZbvvl9Kz1G',
+  access_token_key: '959946187171229696-ctjn6d67dPeZ2KO79Sf8Ak1E5IXWHXN',
+  access_token_secret: '5CwqaYnUa2puuHxqqKlOWuPoriouDfN0e6C4dcPoGwuTk',
 }
 
 module.exports = twitterKeys;
